912ae6ca9b7adb0044f418b98e430519712d9005
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-02-03 21:02:06 -0500

    Set up GET request for recent and alltime leaders

--- src/components/LeaderboardList.js
+++ src/components/LeaderboardList.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import LeaderboardDetail from './LeaderboardDetail';
+import axios from 'axios';
 
 class LeaderboardList extends Component {
 	constructor(props) {
 		super(props);
+
+		var recentUrl = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
+		var allTimeUrl = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+
+		this.state = { recentLeaders: [], allTimeLeaders: [] };
+	}
+
+	getRecentLeaders() {
+		axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+			.then(function(response) {
+				console.log("RECENT");
+				console.log(response.data);
+		});
+	}
+
+	getAllTimeLeaders() {
+		axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+			.then(function(response) {
+				console.log("ALLTIME");
+				console.log(response.data);
+		});
+	}
+
+	componentDidMount() {
+		this.getRecentLeaders();
+		this.getAllTimeLeaders();
 	}
 
 	render() {
 		return (
 			<div className="camper-leaderboard">
 			  <p>Camper Leaderboard List</p>
+			  <p>{this.state.recentLeaders}</p>
 			  <LeaderboardDetail />
 			  <LeaderboardDetail />
 			  <LeaderboardDetail />
